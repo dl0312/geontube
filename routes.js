07ede03922889cf89b0b08600c92116cf2fb5f20
GeonLee <leegun2003@gmail.com>
2019-02-16 18:00:01 +0900

    search

--- routes.js
+++ routes.js
@@ -6,12 +6,14 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
+
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 // Videos
+
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
