65de4390208cf5a9c9441beb2478b667ee600fc0
GeonLee <leegun2003@gmail.com>
2019-02-16 20:35:59 +0900

    video model

--- db.js
+++ db.js
@@ -1,50 +1,17 @@
-export const videos = [
-  {
-    id: 31234,
-    title: "Video awesome",
-    description: "what a amazing video",
-    views: 12314,
-    videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-    creator: {
-      id: 121212,
-      name: "Geon",
-      email: "leegun2003@gmail.com"
-    }
-  },
-  {
-    id: 31234,
-    title: "Video amazing",
-    description: "what a amazing video",
-    views: 12314,
-    videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-    creator: {
-      id: 121212,
-      name: "Geon",
-      email: "leegun2003@gmail.com"
-    }
-  },
-  {
-    id: 31234,
-    title: "Video super",
-    description: "what a amazing video",
-    views: 12314,
-    videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-    creator: {
-      id: 121212,
-      name: "Geon",
-      email: "leegun2003@gmail.com"
-    }
-  },
-  {
-    id: 31234,
-    title: "Video nice",
-    description: "what a amazing video",
-    views: 12314,
-    videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-    creator: {
-      id: 121212,
-      name: "Geon",
-      email: "leegun2003@gmail.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error =>
+  console.log(`❌  Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
